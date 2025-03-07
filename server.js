eb77f3451f9689455face52174f0a9a65b7c5ed9
Dewitt Buckingham <dewitt@zerodaedal.us>
2025-03-06 23:41:06 -0800

    feat(server): :sparkles: Add request logging middleware and enhance server startup logs

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
 import express from "express";
 import cors from "cors";
 import routes from "./server/routes/index.mjs";
+import logger from "./server/middleware/logger.mjs";
 
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || "localhost";
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/", logger);
 
-app.use("/api", (req, res, next) => {
-  console.log(`${req.method} request on ${req.path}`);
-  next();
-});
-
-app.get("/api", (req, res) => {
-  res.status(200).json({ message: "Hey!" });
+// App Routes
+app.get("/api/test", (req, res) => {
+  res.send({ message: "API is functional." });
 });
 
 app.use("/api/customers", routes.customers);
@@ -33,8 +32,16 @@ app.use((err, req, res, next) => {
   res.status(500).send("Misadventure has occurred!");
 });
 
+// Start up Server
 app.listen(PORT, () => {
-  console.log("== Hostname:", HOST);
-  console.log("== Port:", PORT);
+  console.log("\n\nStarting ZeroDae Portal Server...");
+  console.group("====================\n");
+  console.log("NODE_ENV:", process.env.NODE_ENV);
+  console.log("Hostname (Default: localhost):", HOST);
+  console.log("Port (Default: 3000):", PORT);
+  console.groupEnd();
+  console.log("\n====================");
+  console.group();
   console.log("\nLISTENING...\n");
+  console.groupEnd();
 });
